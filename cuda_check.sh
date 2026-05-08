9a80135d8cef965ecf66e259f4b7380688a1c1c5
Andrew W. Steiner <awsteiner0@protonmail.com>
2026-05-08 10:31:45 -0700

    Updating check files.

---
+++ cuda_check.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "pip_audit:"
+pip-audit
+echo ""
+echo -n "TensorFlow import: "
+python3 -c "import tensorflow; tf=open('tfv.txt','w'); tf.write(tensorflow.__version__); tf.close()"
+echo ""
+echo "Output of 'nvidia-smi':"
+nvidia-smi
+echo ""
+grep DESCRIPTION /etc/lsb-release | cut -c22-40
+echo -n "PATH "
+echo $PATH
+echo -n "LD_LIBRARY_PATH "
+echo $LD_LIBRARY_PATH
+echo -n "gcc "
+gcc --version | head -n 1 | awk '{print $3}'
+echo -n "Python "
+python3 --version | awk '{print $2}'
+echo -n "numpy "
+python3 -c "import numpy; print(numpy.__version__);"
+echo -n "HDF5 "
+python3 -c "import h5py; print(h5py.version.hdf5_version)"
+echo -n "h5py "
+python3 -c "import h5py; print(h5py.__version__);"
+echo -n "nvcc "
+nvcc --version
+echo -n "Torch "
+python3 -c "import torch; print(torch.__version__); "
+echo -n "TensorFlow "
+cat tfv.txt
